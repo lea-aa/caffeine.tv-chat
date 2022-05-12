6a998d3a02ded2b18180ea8b25b157f3ed53b57f
Leandro Espósito <leandro-joa@hotmail.com>
2022-05-12 16:26:53 -0300

    always same color for same user

--- addchat.js
+++ addchat.js
@@ -260,17 +260,39 @@ javascript:(function(){
 
         function get_usuario_color(nombre_usuario){
             if(!(nombre_usuario in usuarios_colores)){
-                usuarios_colores[nombre_usuario] = generar_color();
+                usuarios_colores[nombre_usuario] = generar_color(nombre_usuario);
             }
 
             return usuarios_colores[nombre_usuario];
         }
 
-        function generar_color(){
-            var lightColor='hsl('+Math.floor(Math.random()*361)+',100%,65%)';
+        function generar_color(nombre_usuario){
+            /* sacar el "hash" para que sea siempre random, dejarlo para que sea siempre el mismo color para ese usuario pero no se puede elegir */
+            var lightColor='hsl('+generar_numero(nombre_usuario, "hash")+',100%,65%)';
 
             return lightColor;
         }
+
+        function generar_numero(nombre_usuario, tipo = undefined){
+            if (tipo == "hash"){
+                return cyrb53(nombre_usuario) % 361;
+            }
+            else{
+                return Math.floor(Math.random()*361);
+            }
+        }
+
+        const cyrb53 = function(str, seed = 0) {
+            let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+            for (let i = 0, ch; i < str.length; i++) {
+                ch = str.charCodeAt(i);
+                h1 = Math.imul(h1 ^ ch, 2654435761);
+                h2 = Math.imul(h2 ^ ch, 1597334677);
+            }
+            h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+            h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+            return 4294967296 * (2097151 & h2) + (h1>>>0);
+        };
 
         var usuarios_colores = {};
 
